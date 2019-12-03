bb52e9998b100acaefff5cea266a991a2b0a608c
贾新 <1623416514@qq.com>
2019-12-03 17:58:31 +0800

    fix 3-2 路由配置

--- client/app.js
+++ client/app.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './views/App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './views/App'
 import { AppContainer } from 'react-hot-loader'
 
 const root = document.getElementById('root')
 const render = Component => {
-    ReactDOM.hydrate(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        root
-    )
+  ReactDOM.hydrate(
+    <AppContainer>
+      <BrowserRouter >
+        <Component />
+      </BrowserRouter>
+    </AppContainer>,
+    root
+  )
 }
 render(App)
 if (module.hot) {
-    module.hot.accept('./views/App.jsx', () => {
-        const NextApp = require('./views/App.jsx').default
-        render(NextApp)
-    })
+  module.hot.accept('./views/App', () => {
+    const NextApp = require('./views/App').default
+    render(NextApp)
+  })
 }
--- client/views/App.jsx
+++ client/views/App.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import {
+    Link,
+} from 'react-router-dom'
+import Route from '../cofig/router'
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        This is App123
+    componentDidMount() {
+        // do something here
+    }
+    render() {
+        return (
+            <div>
+                <Link to='/' >首页</Link><br />
+                <Link to='/detail'>详情页</Link>
+                <Route />
             </div>
-    )
-  }
+        )
+    }
 }
