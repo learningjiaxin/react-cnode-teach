78ce2389f8a2c3a6556b6ee33d8a809a31fbf17b
贾新 <1623416514@qq.com>
2020-01-10 16:35:40 +0800

    fix 高阶组件refs 字符串

--- client/views/hoc/hocComponent.jsx
+++ client/views/hoc/hocComponent.jsx
@@ -8,11 +8,21 @@ const hocComponent = (WrappedComponent) => {
 
     proc = (instance) => {
       instance.say()
-      console.log(instance.props.msg, 'msg')
+    }
+
+    handleClick = () => {
+      console.log(this.refs.subComp.props)
     }
 
     render() {
-      return <WrappedComponent {...this.props} ref={this.proc} />
+      return (
+        <div>
+          <button type="button" onClick={this.handleClick.bind(this)}>
+            click me
+          </button>
+          <WrappedComponent {...this.props} ref='subComp' />
+        </div>
+      )
     }
   }
   return HocComponent
